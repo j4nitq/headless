aaf98fd15188f95ea5bb4eb3f96922665bac3e5c
j4nitq <111405899+j4nitq@users.noreply.github.com>
2022-08-16 22:58:59 -0400

    Update headless.lua

--- headless.lua
+++ headless.lua
@@ -6,11 +6,4 @@ script.Parent.MouseButton1Click:Connect(function()
 	player.Head.Head:Destroy()
 	player.Head.face:Destroy()
 	player.Head.Transparency = 1
-	--(Remove Accessories)
-	--local d = player:GetChildren()
-	--for i=1, #d do
-		--if (d[i].className == "Accessory") then
-			--d[i]:remove()
-		--end
-	--end
 end)
